5b966f602aab350ed5b66607d799064c3ca830c7
dee-diaz <diazthediablo@yahoo.com>
2025-12-04 12:32:47 -0300

    Create shapes of classes

--- linkedList.js
+++ linkedList.js
@@ -1,3 +1,44 @@
-class LinkedList {}
+class LinkedList {
+  #head;
+  #tail;
+  #size;
+  constructor() {
+    this.#head = null;
+    this.#tail = null;
+    this.#size = 0;
+  }
 
-class Node {}
+  append(value) {}
+
+  prepend(value) {}
+
+  size() {
+    console.log(this.#size);
+    return this.#size;
+  }
+
+  head() {}
+
+  tail() {}
+
+  at(index) {}
+
+  pop() {}
+
+  contains(value) {}
+
+  find(value) {}
+
+  toString() {}
+
+  insertAt(value, index) {}
+
+  removeAt(index) {}
+}
+
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
